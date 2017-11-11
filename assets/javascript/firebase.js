39cdbbe7e28222f5af0200a3f8d15735fcbef013
Darren Evans <darrenrevans10@gmail.com>
2017-11-11 16:29:56 -0500

    updated files

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -5,27 +5,28 @@
     authDomain: "garlic-a9fc8.firebaseapp.com",
     databaseURL: "https://garlic-a9fc8.firebaseio.com",
     projectId: "garlic-a9fc8",
-    storageBucket: "",
+    storageBucket: "garlic-a9fc8.appspot.com",
     messagingSenderId: "196444623528"
   };
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
-  $("#grabDiv").on("click", function(event) {
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
 
   	var firstName = $("#first_name").val().trim();
   	var lastName = $("#last_name").val().trim();
   	var recipe = $("#recipeTried").val().trim();
   	var comment = $("#comment").val().trim();
-  	var rating = $("#rating").val().trim();
+  	// var rating = $("#rating").val().trim();
 
   	var review = {
   		firstName: firstName,
   		lastName: lastName,
   		recipe: recipe,
   		comment: comment,
-  		rating: rating
+  		// rating: rating
   	};
 
   	database.ref().push(review);
@@ -34,7 +35,7 @@
   	$("#last_name").val("");
   	$("#recipeTried").val("");
   	$("#comment").val("");
-  	$("#rating").val("");
+  	// $("#rating").val("");
 
   });
 
@@ -44,10 +45,10 @@
   	var lastName = childSnapshot.val().lastName;
   	var recipe = childSnapshot.val().recipe;
   	var comment = childSnapshot.val().comment;
-  	var rating = childSnapshot.val().rating;
+  	// var rating = childSnapshot.val().rating;
 
   	$("#reviewDisplay").append("<p>" + firstName + " " + lastName + "</p><br>" +
-  							   "<p>" + recipe + " " + rating + "</p><br>" +
+  							   "<p>" + recipe + " "  + "</p><br>" +
   							   "<p>" + comment + "</p>");
 
   }, function(errorObject) {
