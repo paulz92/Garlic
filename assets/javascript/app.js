5e8c8ced5085b7cb2ecb0e9f3614779370aa6dd5
Paul Zavattieri <paulz92@gmail.com>
2017-11-13 11:23:38 -0500

    autoplay carousel

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
+// full width for carousel slider
 $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+// autoplay the carousel images
+function autoplay() {
+	$('.carousel').carousel('next');
+	setTimeout(autoplay, 4000);
+}
+
+autoplay();
 
 
 $('select').material_select();
