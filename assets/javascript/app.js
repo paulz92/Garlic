1152609a8172832dd93d0de112ca3e41923af69c
Paul Zavattieri <paulz92@gmail.com>
2017-11-14 20:07:50 -0500

    replacement app code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
 
-// full width for carousel slider
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+	// full width for carousel slider
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
 
-// autoplay the carousel images
-function autoplay() {
-	$('.carousel').carousel('next');
-	setTimeout(autoplay, 4000);
-}
+	// autoplay the carousel images
+	function autoplay() {
+		$('.carousel').carousel('next');
+		setTimeout(autoplay, 4000);
+	}
 
-autoplay();
+	autoplay();
 
 
-$('select').material_select();
+	$('select').material_select();
 
-$('input[type="checkbox"]').on('change', function() {
-    $('input[name="' + this.name + '"]').not(this).prop('checked', false);
+	$('input[type="checkbox"]').on('change', function() {
+	    $('input[name="' + this.name + '"]').not(this).prop('checked', false);
+	});
+
+	$.fn.stars = function() {
+    return $(this).each(function() {
+      // Get the value
+      var val = parseFloat($(this).html());
+      // Make sure that the value is in 0 - 5 range, multiply to get width
+      var size = Math.max(0, (Math.min(5, val))) * 16;
+      // Create stars holder
+      var $span = $('<span />').width(size);
+      // Replace the numerical value with stars
+      $(this).html($span);
+    });
+	}
+
+	$(function() {
+    $('span.stars').stars();
+	});
 });
-
-});
-
